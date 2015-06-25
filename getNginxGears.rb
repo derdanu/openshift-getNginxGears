460005f91c9a2b7449e26f8071acbd256052aa22
Daniel Falkner <daniel.falkner@telekom.de>
2015-06-25 10:26:17 +0200

    resolv ip address

--- getNginxGears.rb
+++ getNginxGears.rb
@@ -1,17 +1,19 @@
 #!/usr/bin/env oo-ruby
 require "json"
+require "resolv"
 
-registry_file =	ENV["OPENSHIFT_HOMEDIR"] + "gear-registry/gear-registry.json"
+registry_file = ENV["OPENSHIFT_HOMEDIR"] + "gear-registry/gear-registry.json"
 nginx_urls = Array.new
 
 file = open(registry_file)
 json = file.read
-parsed = JSON.parse(json)
+parsed = JSON.parse(json) 
 
 parsed["web"].each do |(key,data)|
-        nginx_urls <<  data["proxy_hostname"] + ":" + data["proxy_port"]
+	ip = Resolv.getaddress data["proxy_hostname"]
+	port = data["proxy_port"]
+        nginx_urls <<  ip + ":" + port
 end
 
 puts nginx_urls.join(",")
-
 
