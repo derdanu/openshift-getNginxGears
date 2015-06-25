32edc44d1ec6371cc4a1b10e2d34074dc6a7d9e2
Daniel Falkner <daniel.falkner@telekom.de>
2015-06-25 11:35:50 +0200

    blank line

--- getNginxGears.rb
+++ getNginxGears.rb
@@ -8,6 +8,7 @@ nginx_urls = Array.new
 file = open(registry_file)
 json = file.read
 parsed = JSON.parse(json) 
+
 parsed["web"].each do |(key,data)|
 	ip = Resolv.getaddress data["proxy_hostname"]
 	port = data["proxy_port"]
